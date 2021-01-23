fee9cd57dfe731f7c60aaa4b984bfc198133d25f
RustedTerrier <RustedTerrier@tutanota.com>
2021-01-23 11:32:52 -0500

    feat: fixed render function

--- src/main.rs
+++ src/main.rs
@@ -93,41 +93,42 @@ fn render(display: bool, colony: &Vec<Colony>,){
             }
         }
     }else{
-        //Will be taken from the vector later...
+        //Print the top colonies
+        print!(" {one}/##\\ {two}/##\\\n\r{reset}             ###\n\r",one = color::Fg(color::Red),two = color::Fg(color::Blue),reset = color::Fg(color::Reset));
+        //Initialize a loop that reads from the vector and renders it.
         let mut i = 0;
         while i < 4 {
             let colony_s = &colony[i];
 
-            print!(" {one}/##\\ {two}/##\\\n\r{reset}",one = color::Fg(color::Red),two = color::Fg(color::Blue),reset = color::Fg(color::Reset));
             if colony_s.selected == 0 {
                 if colony_s.owner == 0{
-                    println!("             ###\n\r<#########>  # #\n\r             ###\n\r");
+                    println!("<#########>  # #\n\r             ###\r");
                 }else{
                     if colony_s.owner == 1{
-                        println!("             ###\n\r{red}<#########>{reset}  #{red}1{reset}#\n\r             ###\n\r",red = color::Fg(color::Red),reset = color::Fg(color::Reset));
+                        println!("{red}<#########>{reset}  #{red}1{reset}#\n\r             ###\r",red = color::Fg(color::Red),reset = color::Fg(color::Reset));
                     }else{
-                        println!("             ###\n\r{blue}<#########>{reset}  #{blue}1{reset}#\n\r             ###\n\r",blue = color::Fg(color::Blue),reset = color::Fg(color::Reset));
+                        println!("{blue}<#########>{reset}  #{blue}2{reset}#\n\r             ###\r",blue = color::Fg(color::Blue),reset = color::Fg(color::Reset));
                     };
                 };
             }else{
                 if colony_s.selected == 1 {
                     if colony_s.owner == 0{
-                        println!("             ###\n\r<#########>  # #\n\r             ###\n\r");
+                        println!("<#########>  # #\n\r             ###\r");
                     }else{
                         if colony_s.owner == 1{
-                            println!("             ###\n\r<#########>  #{red}1{reset}#\n\r             ###\n\r",red = color::Fg(color::Red),reset = color::Fg(color::Reset));
+                            println!("<#########>  #{red}1{reset}#\n\r             ###\r",red = color::Fg(color::Red),reset = color::Fg(color::Reset));
                         }else{
-                            println!("             ###\n\r<#########>  #{blue}1{reset}#\n\r             ###\n\r",blue = color::Fg(color::Blue),reset = color::Fg(color::Reset));
+                            println!("<#########>  #{blue}2{reset}#\n\r             ###\r",blue = color::Fg(color::Blue),reset = color::Fg(color::Reset));
                         };
                     }; 
                 }else{
                     if colony_s.owner == 0{
-                        println!("             ###\n\r{red}<#########>{reset}  # #\n\r             ###\n\r",red = color::Fg(color::Red),reset = color::Fg(color::Reset));
+                        println!("{red}<#########>{reset}  # #\n\r             ###\r",red = color::Fg(color::Red),reset = color::Fg(color::Reset));
                     }else{
                         if colony_s.owner == 1{
-                            println!("             ###\n\r{red}<#########>{reset}  #{red}1{reset}#\n\r             ###\n\r",red = color::Fg(color::Red),reset = color::Fg(color::Reset));
+                            println!("{red}<#########>{reset}  #{red}1{reset}#\n\r             ###\r",red = color::Fg(color::Red),reset = color::Fg(color::Reset));
                         }else{
-                            println!("             ###\n\r{red}<#########>{reset}  #{blue}1{reset}#\n\r             ###\n\r",red = color::Fg(color::Red),blue = color::Fg(color::Blue),reset = color::Fg(color::Reset));
+                            println!("{red}<#########>{reset}  #{blue}2{reset}#\n\r             ###\r",red = color::Fg(color::Red),blue = color::Fg(color::Blue),reset = color::Fg(color::Reset));
                         };
                     };
                 };
